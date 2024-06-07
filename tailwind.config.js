ed36ab43c8fe0572a14ed070ef888c4760a798a2
ekgus9701@gmail.com <52192706+ekgus9701@users.noreply.github.com>
2024-06-07 23:25:37 +0900

    chore: tailwind.config.js colors 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,29 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      CF4F3FA: "#F4F3FA",
-      CECF0FF: "#ECF0FF",
-      C99AAFF: "#99AAFF",
-      C748BFF: "#748BFF",
-      C8DBDFF: "#8DBDFF",
-      CAF9FF3: "#AF9FF3",
-      C333333: "#333333",
-      CD9D9D9: "#D9D9D9",
-    },
+    // colors: {
+    //   CF4F3FA: "#F4F3FA",
+    //   CECF0FF: "#ECF0FF",
+    //   C99AAFF: "#99AAFF",
+    //   C748BFF: "#748BFF",
+    //   C8DBDFF: "#8DBDFF",
+    //   CAF9FF3: "#AF9FF3",
+    //   C333333: "#333333",
+    //   CD9D9D9: "#D9D9D9",
+    // },
 
-    extend: {},
+    extend: {
+      colors: {
+        CF4F3FA: "#F4F3FA",
+        CECF0FF: "#ECF0FF",
+        C99AAFF: "#99AAFF",
+        C748BFF: "#748BFF",
+        C8DBDFF: "#8DBDFF",
+        CAF9FF3: "#AF9FF3",
+        C333333: "#333333",
+        CD9D9D9: "#D9D9D9",
+      },
+    },
   },
-  plugins: [
-    require("@tailwindcss/line-clamp"),
-    require("@tailwindcss/forms"),
-    require("@tailwindcss/typography"),
-  ],
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
 };
