e05cdb4ef5a84dd5355eae0ab9137bd61aabe765
YeongseoYoo <youngseo1314@sookmyung.ac.kr>
2024-06-10 14:36:26 +0900

    feat: 로그인 페이지 구현

---
+++ src/routes/SignUpPage.tsx
@@ -0,0 +1,13 @@
+import MainLogo from "../assets/mainLogo.svg?react";
+import { Link } from "react-router-dom";
+
+const SignUpPage = () => {
+  return (
+    <>
+      <div>
+        <MainLogo />
+      </div>
+    </>
+  );
+};
+export default SignUpPage;
