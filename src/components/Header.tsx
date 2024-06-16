8ee67f0b706640dd6cff3485e62260580e7db62e
ekgus9701@gmail.com <52192706+ekgus9701@users.noreply.github.com>
2024-06-17 00:22:14 +0900

    feat: Header 뒤로가기 기능 구현

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import SearchBtn from "../assets/search-black.svg";
 import BackBtn from "../assets/back.svg";
 import SettingBtn from "../assets/settings.svg";
+import { useNavigate } from "react-router-dom";
 
 type HeaderProps = {
   text: string;
@@ -10,6 +11,15 @@ type HeaderProps = {
 };
 
 const Header: React.FC<HeaderProps> = ({ text, type, to = -1 }) => {
+  const navigate = useNavigate();
+  const handleBackButtonClick = () => {
+    if (typeof to === "string") {
+      navigate(to);
+    } else if (to === -1) {
+      navigate(-1);
+    }
+  };
+
   if (type === 0) {
     // 가운데 텍스트, 검색버튼 있음
     return (
@@ -27,7 +37,12 @@ const Header: React.FC<HeaderProps> = ({ text, type, to = -1 }) => {
     return (
       <nav className="fixed top-0 left-0 right-0 bg-[#ffffff]">
         <div className="relative flex justify-center items-center h-[6vh]">
-          <img src={BackBtn} alt="Back" className="absolute left-4" />
+          <img
+            src={BackBtn}
+            alt="Back"
+            className="absolute left-4"
+            onClick={handleBackButtonClick}
+          />
           <div className="flex-1 text-center font-semibold text-lg">{text}</div>
         </div>
       </nav>
@@ -39,7 +54,12 @@ const Header: React.FC<HeaderProps> = ({ text, type, to = -1 }) => {
     return (
       <nav className="fixed top-0 left-0 right-0 bg-[#ffffff]">
         <div className="relative flex justify-center items-center h-[6vh]">
-          <img src={BackBtn} alt="Back" className="absolute left-4" />
+          <img
+            src={BackBtn}
+            alt="Back"
+            className="absolute left-4"
+            onClick={handleBackButtonClick}
+          />
           <div className="flex-1 text-center font-semibold text-lg">{text}</div>
           <img src={SettingBtn} alt="Setting" className="absolute right-4" />
         </div>
