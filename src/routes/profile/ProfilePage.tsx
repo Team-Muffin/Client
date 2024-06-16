a3b8d8ef77fa0875f2173159497da72a0c40a4f1
YeongseoYoo <youngseo1314@sookmyung.ac.kr>
2024-06-17 08:20:12 +0900

    feat: 프로필 포트폴리오 UI 구현

--- src/routes/profile/ProfilePage.tsx
+++ src/routes/profile/ProfilePage.tsx
@@ -4,12 +4,63 @@ import ProfileCircle from "../../assets/myprofile.svg?react";
 import Checked from "../../assets/checked.svg?react";
 import MyChallenge from "../../assets/challenge-count.svg?react";
 import Navbar from "../../components/Navbar";
+import MyResponsivePie from "./PieChart";
+import MyResponsiveBar from "./BarChart";
 
 const ProfilePage = () => {
   const [userCategory, setUserCategory] = useState("게시물");
+  const [isFollowing, setIsFollowing] = useState(false);
+
   const categories = ["게시물", "챌린지", "포트폴리오", "보유상품"];
+
+  const data = [
+    {
+      id: "stylus",
+      label: "stylus",
+      value: 276,
+      color: "hsl(219, 70%, 50%)",
+    },
+    {
+      id: "sass",
+      label: "sass",
+      value: 85,
+      color: "hsl(243, 70%, 50%)",
+    },
+    {
+      id: "c",
+      label: "c",
+      value: 430,
+      color: "hsl(164, 70%, 50%)",
+    },
+    {
+      id: "ruby",
+      label: "ruby",
+      value: 98,
+      color: "hsl(103, 70%, 50%)",
+    },
+    {
+      id: "php",
+      label: "php",
+      value: 29,
+      color: "hsl(314, 70%, 50%)",
+    },
+  ];
+
+  const asset = [
+    {
+      id: "networth",
+      입출금: 10,
+      저축: 37,
+      투자: 53,
+    },
+  ];
+
   const handleUserCategoryClick = (selection: string) => {
     setUserCategory(selection);
+  };
+
+  const handleFollowButtonClick = () => {
+    setIsFollowing((prevState) => !prevState);
   };
 
   const selectedUserCategoryCss =
@@ -30,7 +81,7 @@ const ProfilePage = () => {
           <ProfileCircle className="w-[35vw]" />
 
           <div className="flex flex-col items-center mt-[3vh]">
-            <p className="text-base">팔로잉</p>
+            <p className="text-base">팔로우</p>
             <p className="text-xl font-bold">10</p>
           </div>
         </div>
@@ -44,8 +95,11 @@ const ProfilePage = () => {
             <p className="text-base mr-[2vw] mb-[0.3vh]">@gildong</p>
             <p className="text-xs text-[#748BFF]">30대 백수</p>
           </div>
-          <button className="text-base font-semibold text-[#748BFF] bg-[#ECF0FF] rounded-3xl shadow my-[2vh] px-[5vw]">
-            팔로잉
+          <button
+            className="text-base font-semibold text-[#748BFF] bg-[#ECF0FF] rounded-3xl shadow my-[2vh] px-[5vw]"
+            onClick={handleFollowButtonClick}
+          >
+            {isFollowing ? "팔로잉" : "팔로우"}
           </button>
         </div>
       </div>
@@ -66,8 +120,25 @@ const ProfilePage = () => {
           </p>
         ))}
       </div>
+
+      {userCategory === "포트폴리오" && (
+        <div className="flex flex-col px-[4.5vw] py-[2vh]">
+          <p className="text-lg ml-[3vw]">홍길동님의 순자산</p>
+          <p className="text-xl font-bold ml-[3vw]">3000만원대</p>
+          <div style={{ height: "20vh" }}>
+            <MyResponsiveBar data={asset} />
+          </div>
+
+          <div className="flex justify-center">
+            <div style={{ height: "30vh", width: "70vw" }}>
+              <MyResponsivePie data={data} />
+            </div>
+          </div>
+        </div>
+      )}
       <Navbar />
     </>
   );
 };
+
 export default ProfilePage;
