adc239a1885e24acac6d672bdf414eda8e3d5ceb
ekgus9701@gmail.com <52192706+ekgus9701@users.noreply.github.com>
2024-06-18 17:51:12 +0900

    feat: board list api 연결

--- src/libs/apis/board.tsx
+++ src/libs/apis/board.tsx
@@ -1,15 +1,82 @@
 import instance from "./base";
 
-// 게시글
-export async function fetchBoardList(
-  pageNo?: number,
-  size?: number,
-  category?: number,
-  sort?: string,
-  userId?: number
-) {
-  const response = await instance.get(
+// 게시글 리스트 조회
+export async function fetchBoardList(options: {
+  pageNo?: number;
+  size?: number;
+  category?: string; //1: 정보, 2: 재미, 3: 투자, 4: 기업, 5: 고급
+  sort?: string; //최신순, 인기순
+  userId?: number;
+}): Promise<{ data: BoardListResponse["data"] }> {
+  const {
+    pageNo = 0,
+    size = 10,
+    category = "1",
+    sort = "최신순",
+    userId = 1,
+  } = options;
+  const response = await instance.get<BoardListResponse>(
     `/board-service/boards?pageNo=${pageNo}&size=${size}&category=${category}&sort=${sort}&userId=${userId}`
   );
-  return response;
+  return { data: response.data.data };
+}
+
+interface BoardListResponse {
+  success: boolean;
+  message: string;
+  data: {
+    id: number;
+    title: string;
+    summary: string;
+    thumbnail: string | null;
+    createdTime: string;
+    likeCount: number;
+    commentCount: number;
+    authorNickname: string;
+  }[];
+}
+
+// 각 게시글 조회
+export async function fetchBoardDetail(
+  boardId: string
+): Promise<{ data: BoardDetailResponse["data"] }> {
+  const response = await instance.get<BoardDetailResponse>(
+    `/board-service/boards/${boardId}`
+  );
+  return { data: response.data.data };
+}
+
+interface BoardDetailResponse {
+  success: boolean;
+  message: string;
+  data: {
+    title: string;
+    content: string;
+    category: {
+      id: number;
+      name: string;
+    };
+    likeCount: number;
+    commentCount: number;
+    comments: {
+      id: number;
+      content: string;
+      authorId: number;
+      authorName: string;
+      authorProfile: string;
+      replies: {
+        id: number;
+        content: string;
+        authorId: number;
+        authorName: string;
+        authorProfile: string;
+        createdTime: string;
+      }[];
+      createdTime: string;
+    }[];
+    createdTime: string;
+    authorId: number;
+    authorNickname: string;
+    authorProfile: string;
+  };
 }
--- src/routes/board/BoardListPage.tsx
+++ src/routes/board/BoardListPage.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../../components/Header";
 import Dropdown from "../../components/Dropdown";
 import writeButton from "../../assets/write-button.svg";
@@ -10,9 +10,29 @@ import { fetchBoardList } from "../../libs/apis/board";
 export default function BoardListPage() {
   const [userCategory, setUserCategory] = useState("정보");
   const categories = ["정보", "재미", "투자", "기업", "고급"];
+  const [pageNo, setPageNo] = useState(0);
+  const [size, setSize] = useState(10);
+  const [category, setCategory] = useState("1");
+  const [userId, setUserId] = useState(1);
+  const [selectedFilter, setSelectedFilter] = useState("최신순");
+
+  const handleFilterChange = (newFilter: string) => {
+    setSelectedFilter(newFilter);
+  };
 
   const handleUserCategoryClick = (selection: string) => {
     setUserCategory(selection);
+    if (selection === "정보") {
+      setCategory("1");
+    } else if (selection === "재미") {
+      setCategory("2");
+    } else if (selection === "투자") {
+      setCategory("3");
+    } else if (selection === "기업") {
+      setCategory("4");
+    } else {
+      setCategory("5");
+    }
   };
 
   const selectedCategoryCss =
@@ -23,19 +43,37 @@ export default function BoardListPage() {
   // const selectedUserCategoryCss =
   //   "px-[1.2vw] text-C333333 border-solid border-b-2 border-C748BFF text-[0.95rem]";
   // const defaultUserCategoryCss = "px-[1.2vw] text-C333333 text-[0.95rem]";
+  interface BoardData {
+    id: number;
+    title: string;
+    summary: string;
+    thumbnail: string | null;
+    createdTime: string;
+    likeCount: number;
+    commentCount: number;
+    authorNickname: string;
+  }
 
-  const [boardData, setBoardData] = useState([]);
+  const [boardData, setBoardData] = useState<BoardData[]>([]);
   const callBoardData = async () => {
     try {
-      const response = await fetchBoardList(1, 10, 1, "popular");
-      console.log(response);
+      const { data } = await fetchBoardList({
+        pageNo: pageNo,
+        size: size,
+        category: category,
+        sort: selectedFilter,
+        userId: userId,
+      });
+      setBoardData(data);
+      console.log({ data });
     } catch (error) {
       console.error("보드 데이터 호출 중 에러:", error);
     }
   };
+
   useEffect(() => {
     callBoardData();
-  }, []);
+  }, [pageNo, size, category, selectedFilter, userId]);
 
   return (
     <div className="py-[2vh] px-[4.5vw]">
@@ -58,89 +96,30 @@ export default function BoardListPage() {
         ))}
       </div>
 
-      <Dropdown defaultFilter="인기순" filterList={["인기순", "최신순"]} />
+      <Dropdown
+        defaultFilter="최신순"
+        filterList={["최신순", "인기순"]}
+        onFilterChange={handleFilterChange}
+      />
 
       <hr className="border-CD9D9D9 mt-[1vh]" />
-      <BoardCard
-        title="⭐️내가 들었던 펀드 추천 글⭐️"
-        description=" 오늘은 내가 들었던 펀드 중에 가장 좋았던 펀드는 신한은행의"
-        author="이듀미"
-        time="3"
-        heartCount={7}
-        replyCount={3}
-        imageUrl="https://img1.daumcdn.net/thumb/R658x0.q70/?fname=https://t1.daumcdn.net/news/202105/25/holapet/20210525044423699dwdp.jpg"
-        link="board/1"
-      />
-      <BoardCard
-        title="⭐️내가 들었던 펀드 추천 글⭐️"
-        description=" 오늘은 내가 들었던 펀드 중에 가장 좋았던 펀드는 신한은행의"
-        author="이듀미"
-        time="3"
-        heartCount={7}
-        replyCount={3}
-        imageUrl="https://img1.daumcdn.net/thumb/R658x0.q70/?fname=https://t1.daumcdn.net/news/202105/25/holapet/20210525044423699dwdp.jpg"
-        link=""
-      />
-      <BoardCard
-        title="⭐️내가 들었던 펀드 추천 글⭐️"
-        description=" 오늘은 내가 들었던 펀드 중에 가장 좋았던 펀드는 신한은행의"
-        author="이듀미"
-        time="3"
-        heartCount={7}
-        replyCount={3}
-        imageUrl="https://img1.daumcdn.net/thumb/R658x0.q70/?fname=https://t1.daumcdn.net/news/202105/25/holapet/20210525044423699dwdp.jpg"
-        link=""
-      />
-      <BoardCard
-        title="⭐️내가 들었던 펀드 추천 글⭐️"
-        description=" 오늘은 내가 들었던 펀드 중에 가장 좋았던 펀드는 신한은행의"
-        author="이듀미"
-        time="3"
-        heartCount={7}
-        replyCount={3}
-        imageUrl="https://img1.daumcdn.net/thumb/R658x0.q70/?fname=https://t1.daumcdn.net/news/202105/25/holapet/20210525044423699dwdp.jpg"
-        link=""
-      />
-      <BoardCard
-        title="⭐️내가 들었던 펀드 추천 글⭐️"
-        description=" 오늘은 내가 들었던 펀드 중에 가장 좋았던 펀드는 신한은행의"
-        author="이듀미"
-        time="3"
-        heartCount={7}
-        replyCount={3}
-        imageUrl="https://img1.daumcdn.net/thumb/R658x0.q70/?fname=https://t1.daumcdn.net/news/202105/25/holapet/20210525044423699dwdp.jpg"
-        link=""
-      />
-      <BoardCard
-        title="⭐️내가 들었던 펀드 추천 글⭐️"
-        description=" 오늘은 내가 들었던 펀드 중에 가장 좋았던 펀드는 신한은행의"
-        author="이듀미"
-        time="3"
-        heartCount={7}
-        replyCount={3}
-        imageUrl="https://img1.daumcdn.net/thumb/R658x0.q70/?fname=https://t1.daumcdn.net/news/202105/25/holapet/20210525044423699dwdp.jpg"
-        link=""
-      />
-      <BoardCard
-        title="⭐️내가 들었던 펀드 추천 글⭐️"
-        description=" 오늘은 내가 들었던 펀드 중에 가장 좋았던 펀드는 신한은행의"
-        author="이듀미"
-        time="3"
-        heartCount={7}
-        replyCount={3}
-        imageUrl="https://img1.daumcdn.net/thumb/R658x0.q70/?fname=https://t1.daumcdn.net/news/202105/25/holapet/20210525044423699dwdp.jpg"
-        link=""
-      />
-      <BoardCard
-        title="⭐️내가 들었던 펀드 추천 글⭐️"
-        description=" 오늘은 내가 들었던 펀드 중에 가장 좋았던 펀드는 신한은행의"
-        author="이듀미"
-        time="3"
-        heartCount={7}
-        replyCount={3}
-        imageUrl="https://img1.daumcdn.net/thumb/R658x0.q70/?fname=https://t1.daumcdn.net/news/202105/25/holapet/20210525044423699dwdp.jpg"
-        link=""
-      />
+      {boardData.map((data, index) => (
+        <React.Fragment key={index}>
+          <BoardCard
+            title={data.title}
+            description={data.summary}
+            author={data.authorNickname}
+            time={data.createdTime}
+            heartCount={data.likeCount}
+            replyCount={data.commentCount}
+            imageUrl="https://png.pngtree.com/png-vector/20190411/ourmid/pngtree-vector-business-men-icon-png-image_925963.jpg"
+            // imageUrl={data.thumbnail}
+            authorImageUrl="https://png.pngtree.com/png-vector/20190411/ourmid/pngtree-vector-business-men-icon-png-image_925963.jpg"
+            link={`board/${data.id}`}
+          />
+        </React.Fragment>
+      ))}
+
       <Link to={`/boardWrite`}>
         <img className="fixed bottom-[8vh] right-[4vw] z-5" src={writeButton} />
       </Link>
