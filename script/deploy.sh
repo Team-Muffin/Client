52c1c4cb50afcdbdaff5a3a76a7f89b6743f278f
bk <roboticts49@gmail.com>
2024-06-21 14:27:35 +0900

    chore: clean existing static files

--- script/deploy.sh
+++ script/deploy.sh
@@ -13,6 +13,7 @@ docker pull bkkmw/${IMAGE_NAME}
 echo 'execute container'
 docker run --name ${IMAGE_NAME} -v ./output:/output -d bkkmw/${IMAGE_NAME}
 
+rm -rf /home/ubuntu/ws/nginx/html/
 cp -rfT ./output/ /home/ubuntu/ws/nginx/html/
 
 echo 'remove container'
