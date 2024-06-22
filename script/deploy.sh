a537c19686af8cfa4a1f1f2bea06a341b6ea3ed2
bkkmw <roboticts49@gmail.com>
2024-06-22 10:21:04 +0900

    fix: clean existing file from workspace before deploy

--- script/deploy.sh
+++ script/deploy.sh
@@ -10,9 +10,14 @@ IMAGE_NAME=$1
 echo 'pull docker image'
 docker pull bkkmw/${IMAGE_NAME}
 
+echo 'clean existing file'
+rm -rf ./output/
+
 echo 'execute container'
+mkdir output
 docker run --name ${IMAGE_NAME} -v ./output:/output -d bkkmw/${IMAGE_NAME}
 
+echo 'remove existing files'
 rm -rf /home/ubuntu/ws/nginx/html/
 cp -rfT ./output/ /home/ubuntu/ws/nginx/html/
 
